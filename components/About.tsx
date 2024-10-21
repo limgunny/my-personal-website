b5d2633d4efec1da4a0e8e7799e252a87968435d
limgunny <red373712345@gmail.com>
2024-10-21 22:14:30 +0900

    1

--- components/About.tsx
+++ components/About.tsx
@@ -12,23 +12,22 @@ export const About = () => {
         안녕하세요 저는 중부대학교 정보보호학과에 재학 중인 임건희입니다.
       </Text>
       <Text style={{ marginBottom: `${TEXT_MARGIN_BOTTOM}rem` }}>
-        I use a variety of technologies to ease my development process, when
-        working on the client I use &#38;{' '}
+        현재 포트폴리를 제공하는 이 웹사이트는{' '}
         <LinkTag href="https://nextjs.org/" target="_blank">
           NextJS
         </LinkTag>{' '}
-        and for the serverside I use{' '}
-        <LinkTag href="https://nodejs.org/en/" target="_blank">
-          Node
+        를 기반으로 만들어 졌으며 차후{' '}
+        <LinkTag href="https://www.mongodb.com/ko-kr" target="_blank">
+          mongoDB
         </LinkTag>{' '}
         &#38;{' '}
-        <LinkTag href="https://php.net/" target="_blank">
-          PHP
+        <LinkTag href="https://thirdweb.com/" target="_blank">
+          ThirdWeb
         </LinkTag>
+        을 활용하여 포트폴리오를 보다 완성적으로 채워나가겠습니다.
       </Text>
       <Text style={{ marginBottom: `${TEXT_MARGIN_BOTTOM}rem` }}>
-        Besides programming, I like to listen to music, travel in my spare time
-        and go out with my friends.
+        프로그래밍 이외에도 저는 노래를 듣거나 영화를 보는것을 좋아합니다
       </Text>
     </section>
   )
